2f2fdcd07c855e315d9aa84b4c0c654438ca6bc8
Anthony McGarry <antjmcgarry@gmail.com>
2023-10-22 15:38:41 +0100

    created  modal

--- client/src/components/list-header/index.tsx
+++ client/src/components/list-header/index.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { Modal } from "../modal";
+
 interface ListHeaderProps {
   listName: string;
 }
@@ -7,12 +10,16 @@ const onSignOut = () => {
 };
 
 const ListHeader = ({ listName }: ListHeaderProps) => {
+  const [showModal, setShowModal] = useState(false);
   return (
     <div className="flex justify-between items-center	p-4 border-b-4 border-slate-500">
       <h1 className="text-6xl">{listName}</h1>
       <div />
       <div className="flex flex-wrap justify-center gap-4 p-4">
-        <button className="flex sm:inline-flex justify-center items-center bg-blue-500 hover:bg-blue-600 active:bg-blue-700 focus-visible:ring ring-blue-300 text-white font-semibold text-center rounded-md outline-none transition duration-100 px-5 py-2">
+        <button
+          className="flex sm:inline-flex justify-center items-center bg-blue-500 hover:bg-blue-600 active:bg-blue-700 focus-visible:ring ring-blue-300 text-white font-semibold text-center rounded-md outline-none transition duration-100 px-5 py-2"
+          onClick={() => setShowModal((prev) => !prev)}
+        >
           Add New
         </button>
         <button
@@ -22,6 +29,11 @@ const ListHeader = ({ listName }: ListHeaderProps) => {
           Sign Out
         </button>
       </div>
+      <Modal
+        isOpen={showModal}
+        onClose={() => setShowModal(false)}
+        title="Add New TODO"
+      />
     </div>
   );
 };

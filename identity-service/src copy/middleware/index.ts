51c06dae9fbd6bae3335fabb0124d28322e1dede
Anthony McGarry <antjmcgarry@gmail.com>
2024-03-31 19:04:23 +0100

    incorrect copy

--- identity-service/src copy/middleware/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as createParamValidator } from './create-param-validator';
-export { default as createBodyValidator } from './create-body-validator';
-export { default as createQueryValidator } from './create-query-validator';
-export { default as VerifyAuth } from './verify-auth';
--- identity-service/src copy/validation/login-user.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-const loginUserSchema = z.object({
-  email: z.string().email({ message: 'Invalid email format' }),
-  password: z.string(),
-});
-
-export default loginUserSchema;

0bdd5964aa04b72fd18b64d993290e14ca12f7fb
Anthony McGarry <antjmcgarry@gmail.com>
2024-03-31 19:02:22 +0100

    remove unused code

--- server/src/middleware/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as createParamValidator } from "./createParamValidator";
-export { default as createBodyValidator } from "./createBodyValidator";
-export { default as createQueryValidator } from "./createQueryValidator";
--- server/src/models/todo.ts
+++
@@ -1,8 +0,0 @@
-export interface Todo {
-  id: string;
-  title: string;
-  user_id: string;
-  description: string;
-  progress: number;
-  date: Date;
-}

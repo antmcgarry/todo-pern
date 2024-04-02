aaf45259fb3f697d69bdac0148bc6f983e8aa340
Anthony McGarry <antjmcgarry@gmail.com>
2024-04-02 10:09:46 +0100

    remove old client

--- client/src/components/modal/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as AuthModal } from "./auth";
-export { default as Modal } from "./modal";
--- client/src/services/interfaces/todo.ts
+++
@@ -1,8 +0,0 @@
-export interface ITodo {
-  id: string;
-  title: string;
-  user_id: string;
-  description: string;
-  progress: number;
-  date: Date;
-}
